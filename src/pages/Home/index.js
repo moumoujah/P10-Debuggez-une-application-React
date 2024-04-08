b8945e97fc9216796d5530da35232861f6ba46fd
Bastien Moulin <bastienjah@outlook.fr>
2024-04-08 11:38:49 +0200

    fix: slider lenght

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,8 +22,8 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+      <section className="ServicesContainer" id="nos-services">
+        <h2 className="Title" >Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -52,11 +52,11 @@ const Page = () => {
         </div>
       </section>
       <section className="EventsContainer">
-        <h2 className="Title">Nos réalisations</h2>
+        <h2 className="Title"  id="nos-realisations">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
-        <h2 className="Title">Notre équipe</h2>
+      <section className="PeoplesContainer" id="notre-equipe">
+        <h2 className="Title" >Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
